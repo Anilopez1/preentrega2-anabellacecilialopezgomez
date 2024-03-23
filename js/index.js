0c59a752f0df69940033d6d83891e0e534150425
Anilopez1 <anilopezg@gmail.com>
2024-03-22 22:18:47 -0300

    agrego img

--- js/index.js
+++ js/index.js
@@ -14,3 +14,20 @@ function dropdown() {
   var dropdown = document.querySelector('.dropdown');
   dropdown.classList.toggle('active');
 }
+
+/* Animaciones con ScrollMagic */
+const controller = new ScrollMagic.Controller();
+
+/* Inicio */
+const inicioAnimation = new TimelineMax()
+.from('.inicio__exposiciones', 1, { opacity: 0, y: 50 })
+.from('.inicio__imagenes', 1, { opacity: 0, y: 50 }, '-=0.5');
+
+new ScrollMagic.Scene({
+  triggerElement: '.inicio',
+  triggerHook: 0.8,
+  reverse: false
+})
+
+.setTween(inicioAnimation)
+.addTo(controller);
